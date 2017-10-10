dbaa85adddf42898ce833d6ce8b9e80cd0a7adb1
Hugo Antonio Segoviano Martinez <hugo.hasm@gmail.com>
2017-10-10 14:07:39 -0500

    Se actualiza el app.js para que nos de un puerto

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var nombre="Hugo";
 var nick="H117";
 console.log(nombre,'es',nick);*/
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var port=process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
-})
+});
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!')
-})
+});
