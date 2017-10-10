2cddfd8b6c1a6a05d375689ad07516656e33e4dc
Hugo Antonio Segoviano Martinez <hugo.hasm@gmail.com>
2017-10-10 14:02:33 -0500

    Ya existe la logica base del server

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-console.log('Hola mundo!', 'Fuck yeah!');
+/*console.log('Hola mundo!', 'Fuck yeah!');
 var nombre="Hugo";
 var nick="H117";
-console.log(nombre,'es',nick);
+console.log(nombre,'es',nick);*/
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
